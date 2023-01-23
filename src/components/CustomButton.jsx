2789e8a66e461a3f296ed3cb663a9a18e5928bc5
kartikeya sharma <karthiksharma1411@gmail.com>
2023-01-24 01:10:52 +0530

    fixing some things

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,22 @@
+import { Button } from 'react-bootstrap';
+
+const CustomButton = () => {
+    return (
+      <div class="container">
+      <div class="row">
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+      </div>
+    </div>
+  
+    );
+  };
+  
+  export default CustomButton;
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+
+const TopBar = () => {
+    return (
+    <nav class="navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </nav>
+    );
+  };
+  
+  export default TopBar;
